90062ccabd8733ab648d15e463c98faea9926ae4
khairahmard <khairunnisamardiyani@gmail.com>
2017-11-15 04:21:39 +0700

    getattr

--- Soal2.c
+++ Soal2.c
@@ -10,3 +10,16 @@
 
 static const char *dirpath = "/home/khairunnisa/Documents";
 
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+  int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
