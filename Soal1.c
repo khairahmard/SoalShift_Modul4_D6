192c6ce73a3504f3ab742c69c5adc54ffbd99912
khairahmard <khairunnisamardiyani@gmail.com>
2017-11-15 03:39:42 +0700

    include no1

---
+++ Soal1.c
@@ -0,0 +1,9 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
